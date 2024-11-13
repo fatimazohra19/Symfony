bc68307908c4f9963838f141a1c9e4c7dd53a134
dib fatima zohra <dibfatimazohra501@gmail.com>
2024-11-13 16:17:33 +0100

    Nos premières pages

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route("/", name:"home" )]
+    function index(Request $request) : Response {
+
+   // return new Response('Bonjour ' . $_GET['name']);
+   dd($request);
+   return new Response('Bonjour ' . $request-> query ->get('name','Inconnu'));
+
+
+    }
+}
